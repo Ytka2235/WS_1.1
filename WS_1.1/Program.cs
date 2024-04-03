1a5acaaabff883e0b827f79a1e5b98e8acfcd8ca
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2024-04-03 11:20:18 +0300

    Добавьте файлы проекта.

---
+++ WS_1.1/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int P = Convert.ToInt32(Console.ReadLine());
+        int[] p_num = PNum(P);
+        foreach (int i in p_num)
+        {
+            foreach (int j in p_num)
+            {
+                if ((i+j) == P) Console.WriteLine($"({i};{j})");
+            }
+        }
+    }
+
+    static int[] PNum(int x)
+    {
+        List<int> list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(5);
+        for (int i = 2; i <= x; i++)
+        {
+            if (isSimple(i)) list.Add(i);
+        }
+
+        return list.ToArray();
+
+        static bool isSimple(int N)
+        {
+            bool tf = false;
+            for (int i = 2; i < (int)(N / 2); i++)
+            {
+                if (N % i == 0)
+                {
+                    tf = false;
+                    break;
+                }
+                else
+                {
+                    tf = true;
+                }
+            }
+            return tf;
+        }
+    }
+}
